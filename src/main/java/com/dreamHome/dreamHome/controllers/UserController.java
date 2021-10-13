d3c4bef5c5db3f1cb715119b19d27430595c4133
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-10-13 09:19:08 -0500

    Added images

--- src/main/java/com/dreamHome/dreamHome/controllers/UserController.java
+++ src/main/java/com/dreamHome/dreamHome/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.dreamHome.dreamHome.controllers;
 
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,9 +8,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserController {
 
-    @GetMapping("")
+    @GetMapping("/hero")
     public String home(){
         return "hero";
+    }
+
+    @PostMapping("/hero")
+    public String goHome () {
+        return "hero";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/user/profile")
+    public String userProfile() {
+        return "profileUser";
+    }
+
+    @GetMapping("/admin/profile")
+    public String adminProfile() {
+        return "profileAdmin";
     }
 
 
